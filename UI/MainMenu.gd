21ccc98ced86b8116fb5a4cf6e951897b77331f4
Flyn San <flynsarmy@gmail.com>
2022-07-21 21:23:41 +1000

    Add a grappling player for personality

--- UI/MainMenu.gd
+++ UI/MainMenu.gd
@@ -3,9 +3,18 @@ extends Control
 # How fast we want to move the parallax background per second
 onready var bg_move_speed: Vector2 = get_viewport_rect().size / 2
 
+onready var player: RigidBody2D = $Node2D/Player
+onready var wall: StaticBody2D = $Node2D/Wall
+onready var grapple: Line2D = $Node2D/Grapple
+
 func _process(delta: float) -> void:
 	var parallax: ParallaxBackground = $GameBackground
 	parallax.scroll_offset.x -= bg_move_speed.x * delta
+	
+func _physics_process(_delta: float) -> void:
+	grapple.clear_points()
+	grapple.add_point(player.global_position)
+	grapple.add_point(wall.global_position, 1)
 
 func _on_btnSound_toggled(button_pressed: bool) -> void:
 	if button_pressed:
