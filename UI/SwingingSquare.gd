caf4d48dceb1770f57ce2523416ded93830dda91
Flyn San <flynsarmy@gmail.com>
2022-07-29 17:45:55 +1000

    Nice looking unlockables list

---
+++ UI/SwingingSquare.gd
@@ -0,0 +1,79 @@
+extends Node2D
+
+class_name SwingingSquare
+
+# Half the swing arc in radians
+export(float) var max_swing_angle: float = deg2rad(29)
+export(float) var swing_duration: float = 1.5
+export(int) var line_length: int = 275
+export(Dictionary) var avatar: Dictionary = {}
+
+onready var source: Position2D = $SourcePosition
+onready var player: Node2D = $SourcePosition/Player
+onready var grapple: Line2D = $SourcePosition/GrappleLine
+onready var swing_tween: Tween = $SwingTween
+
+func _ready() -> void:
+	randomize()
+
+	if not avatar.has("offset"):
+		avatar = GsCustomizationManager.get_avatar_details()
+
+	# Move player into position
+	player.position.x -= line_length
+	($SourcePosition/Player/Sprite as Sprite).region_rect.position = avatar.get("offset")
+	# Set up the grapple line
+	grapple.default_color = avatar.get("color")
+	grapple.add_point(source.position)
+	grapple.add_point(player.position)
+
+	var start_angle_offset: float = rand_range(-max_swing_angle, max_swing_angle)
+	var percent_through_swing = (max_swing_angle - start_angle_offset) / (max_swing_angle * 2)
+	source.global_rotation = deg2rad(-90) + start_angle_offset
+
+	# The first swing will be offset by a random amount requiring this custom tween
+	var __ = swing_tween.interpolate_property(
+		source,
+		"global_rotation",
+		source.global_rotation,
+		deg2rad(-90) - max_swing_angle,
+		swing_duration,
+		Tween.TRANS_SINE
+	)
+	__ = swing_tween.start()
+	# Seek to the amount we started through the swing so the animation plays at the correct speed.
+	__ = swing_tween.seek(swing_duration * percent_through_swing)
+
+	yield(swing_tween, "tween_completed")
+	swing(false)
+
+
+func swing(is_swinging_forwards: bool) -> void:
+	var start_angle: float = deg2rad(-90) + (max_swing_angle if is_swinging_forwards else -max_swing_angle)
+	var end_angle: float = deg2rad(-90) + (-max_swing_angle if is_swinging_forwards else max_swing_angle)
+
+	var __ = swing_tween.interpolate_property(
+		source,
+		"global_rotation",
+		start_angle,
+		end_angle,
+		swing_duration,
+		Tween.TRANS_SINE
+	)
+	__ = swing_tween.start()
+
+	yield(swing_tween, "tween_completed")
+	swing(!is_swinging_forwards)
+
+func set_avatar_scale(new_scale: float) -> void:
+	var sprite: Sprite = $SourcePosition/Player/Sprite
+	var scale_tween: Tween = $ScaleTween
+
+	var __ = scale_tween.interpolate_property(
+		sprite,
+		"scale",
+		sprite.scale,
+		Vector2(new_scale, new_scale),
+		0.1
+	)
+	__ = scale_tween.start()
--- UI/UnlockableAvatars.gd
+++ UI/UnlockableAvatars.gd
@@ -1,18 +1,82 @@
 extends Control
 
-const AVATAR_SPACING = 150
-const SCN_UNLOCKABLE_AVATAR = preload("res://UI/UnlockableAvatar.tscn")
+const AVATAR_SPACING = 200
+const AVATAR_NORMAL_SCALE = 1
+const AVATAR_SELECTED_SCALE = 3
+const SCN_SKIN = preload("res://UI/SwingingSquare.tscn")
 
 onready var container: Node2D = $AvatarsContainer
+onready var drag_x: float = get_global_mouse_position().x
+onready var tween: Tween = $Tween
+onready var half_screen_width: float = get_viewport_rect().size.x / 2
+
+var min_x: float = 0
+var max_x: float = 0
+var cached_nearest_skin_index: int = -1
+var selected_skin_node: SwingingSquare
 
 func _ready() -> void:
 	var current_avatar = GsCustomizationManager.get_avatar_details()
 
 	var index: int = 0
 	for key in GsCustomizationManager.AVATARS:
-		var node: UnlockableAvatar = SCN_UNLOCKABLE_AVATAR.instance()
-		node.avatar = key
+		var node: SwingingSquare = SCN_SKIN.instance()
+		node.avatar = GsCustomizationManager.get_avatar_details(key)
+		node.max_swing_angle = deg2rad(15)
+		node.line_length = floor(rand_range(180, 220)) # warning-ignore:narrowing_conversion
+		node.swing_duration = 1.2
 		container.add_child(node)
-		node.position = Vector2(AVATAR_SPACING * (index - current_avatar.get("index")), 0)
+		node.position = Vector2(
+			AVATAR_SPACING * (index - current_avatar.get("index")) + half_screen_width,
+			40
+		)
+
+		if current_avatar.get("index") == index:
+			_update_selected_node(node)
+
+		if index == 0:
+			min_x = node.global_position.x
+		else:
+			max_x = max(max_x, node.global_position.x)
 
 		index += 1
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("grapple"):
+		drag_x = get_global_mouse_position().x
+		var __ = tween.stop_all()
+	elif Input.is_action_just_released("grapple"):
+		_pan_to_nearest_skin()
+
+	if Input.is_action_pressed("grapple"):
+		container.global_position.x += get_global_mouse_position().x - drag_x
+		drag_x = get_global_mouse_position().x
+		_set_nearest_skin()
+
+func _set_nearest_skin() -> void:
+	var current_x = -container.global_position.x + half_screen_width
+	var index: int = round((clamp(current_x, min_x, max_x) - min_x) / AVATAR_SPACING) # warning-ignore:narrowing_conversion
+
+	if index != cached_nearest_skin_index:
+		cached_nearest_skin_index = index
+		_update_selected_node(container.get_child(index))
+
+
+# 'Sticks' the camera to the closest skin to the current pan position
+func _pan_to_nearest_skin() -> void:
+	var __ = tween.interpolate_property(
+		container,
+		"global_position:x",
+		container.global_position.x,
+		(selected_skin_node.position.x - half_screen_width) * -1,
+		0.1,
+		Tween.TRANS_CUBIC
+	)
+	__ = tween.start()
+
+func _update_selected_node(new_node: SwingingSquare):
+	if selected_skin_node:
+		selected_skin_node.set_avatar_scale(AVATAR_NORMAL_SCALE)
+
+	selected_skin_node = new_node
+	selected_skin_node.set_avatar_scale(AVATAR_SELECTED_SCALE)
